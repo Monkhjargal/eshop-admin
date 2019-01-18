ff50730d61bb6345990f215480125d2676d43008
Moogii <monkhjargal14@gmail.com>
2019-01-18 16:58:01 +0800

    update

--- src/components/Form/Widgets/SelectTreeWidget/index.js
+++ src/components/Form/Widgets/SelectTreeWidget/index.js
@@ -25,36 +25,17 @@ class SelectTreeWidget extends Component {
 
   renderTreeNodes = data => data.map((item) => {
     if (item.children) {
-      console.log(item.id);
+      // console.log(item.id);
       return (
-        <TreeNode title={item.catnm} dataRef={item} key={item.id + 1} selectable={false} value={`${item.id}`}>
+        <TreeNode title={item.catnm} dataRef={item} key={item.id} value={`${item.id}`}>
           {this.renderTreeNodes(item.children)}
         </TreeNode>
       );
     }
-    return <TreeNode dataRef={item} title={item.catnm} key={item.id + 1000000} selectable={false} value={`${item.id}`} {...item} />;
+    return <TreeNode dataRef={item} title={item.catnm} key={item.id} value={`${item.id}`} {...item} />;
   });
 
   render() {
-    const treeData1 = [{
-      title: 'Node1',
-      value: '0-0',
-      key: '0-0',
-      children: [{
-        title: 'Child Node1',
-        value: '0-0-1',
-        key: '0-0-1',
-      }, {
-        title: 'Child Node2',
-        value: '0-0-2',
-        key: '0-0-2',
-      }],
-    }, {
-      title: 'Node2',
-      value: '0-1',
-      key: '0-1',
-    }];
-
     const root = [];
     this.props.schema.options.forEach((entry, index) => {
       if (entry.parentid === 0) {
@@ -70,15 +51,15 @@ class SelectTreeWidget extends Component {
     // console.log(root);
     return (
       <TreeSelect
-        value={this.state.value}
+        value={this.props.value}
         showSearch
         size="small"
         dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
         treeDefaultExpandAll
         onChange={this.onChange}
-        treeData={treeData1}
+        // treeData={treeData1}
       >
-        {/* {this.renderTreeNodes(root)} */}
+        {this.renderTreeNodes(root)}
       </TreeSelect>
     );
   }
